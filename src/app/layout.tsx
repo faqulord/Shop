0055cf95ad45545dbc5245fed2221325bf3e4f1a
faqulord <stylefaqu@gmail.com>
2026-01-20 06:27:31 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,18 @@
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Lipses Shop",
+  description: "A legjobb dropshipping termékek",
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="hu">
       <body>{children}</body>
     </html>
   )
