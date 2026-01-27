36abd5db10029d0b3688998e08ce9effebba9341
faqulord <stylefaqu@gmail.com>
2026-01-27 13:38:52 +0100

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          light: "#FDF4F5",
+          rose: "#EDBebe",
+          gold: "#C69C6D",
+          dark: "#5A3A3A",
+          accent: "#D3455B",
+        },
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
