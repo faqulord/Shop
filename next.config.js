e993bdd59b709940073095da5648c15c2c2cd213
faqulord <stylefaqu@gmail.com>
2026-01-27 13:38:03 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['img.freepik.com', 'images.unsplash.com'], // Engedélyezzük külső képek betöltését
+    },
+}
+
+module.exports = nextConfig
