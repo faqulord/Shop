cd4febd72d4e48a476c2ca993cc031e97a5df445
faqulord <stylefaqu@gmail.com>
2026-01-27 13:01:39 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
-// Betöltjük a Google Fontokat a luxus kinézethez
-import { Playfair_Display, Montserrat } from "next/font/google"; 
-
-const playfair = Playfair_Display({ subsets: ["latin"], weight: ["400", "700"], variable: '--font-serif' });
-const montserrat = Montserrat({ subsets: ["latin"], weight: ["300", "400", "500", "700"], variable: '--font-sans' });
 
 export const metadata: Metadata = {
-  title: "LipsesHungary | Valentin Napi Akció",
-  description: "Prémium ajakdúsítás tű nélkül.",
+  title: "LipsesHungary - Valentin Napi Nyereményjáték",
+  description: "Prémium ajakápolás tű nélkül.",
 };
 
 export default function RootLayout({
@@ -18,18 +13,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="hu">
-      <body className={`${playfair.variable} ${montserrat.variable} antialiased`}>
-        
+      <body className="antialiased">
         {/* Hóesés réteg */}
         <div className="snow-container">
             <div className="snow"></div>
         </div>
-
-        {/* Tartalom */}
-        <main className="relative z-10 min-h-screen">
-            {children}
-        </main>
-        
+        {children}
       </body>
     </html>
   )
