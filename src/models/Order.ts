9ff064474fb612200e4e10d3013bcd5e8f53f5fa
faqulord <stylefaqu@gmail.com>
2026-01-27 16:35:15 +0100

    Update Order.ts

--- src/models/Order.ts
+++ src/models/Order.ts
@@ -4,18 +4,13 @@ const OrderSchema = new Schema({
   customerName: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
-  address: { type: String, required: true }, // Irányítószám, Város, Utca, Házszám egyben
+  address: { type: String, required: true },
   product: { type: String, default: "Lipses Lip Plumper" },
-  status: { 
-    type: String, 
-    enum: ['uj', 'feldolgozva', 'kiszallitva'], 
-    default: 'uj' 
-  },
-  price: { type: Number, default: 12990 }, // Ár példa
+  status: { type: String, default: 'fizetesre_var' }, // Most már elfogadja ezt is!
+  price: { type: Number, default: 12990 },
   createdAt: { type: Date, default: Date.now },
 });
 
-// Ha már létezik a modell, ne hozza létre újra (Next.js hiba elkerülése)
 const Order = models.Order || model('Order', OrderSchema);
 
 export default Order;
